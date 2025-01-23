c3e49865a641a59db35bd1bbc97c0b1e8bedba27
codepatinga <muddappa@protonmail.com>
2025-01-23 16:03:28 +0530

    v2

--- movie2.py
+++ movie2.py
@@ -45,7 +45,6 @@ def show_random_movie():
     poster_image = get_movie_poster_from_letterboxd(letterboxd_url)
 
     # Display movie info
-    st.title("Quick Movie Recommendation System")
     st.header(f"Title: {movie_title}")
     st.subheader(f"Year: {movie['year']}")
     
